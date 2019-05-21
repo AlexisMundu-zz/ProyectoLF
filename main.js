9d4f44ed6183f779db7d595ea8e68270ca47afc8
Alexis Muñoz <alexismun53@gmail.com>
2019-05-21 13:52:58 -0500

    Integrate modules in main

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 const M1 = require('./m1');
 const M2 = require('./m2');
+const M3 = require('./m3');
+const M4 = require('./m4');
+const M5 = require('./m5');
+const M6 = require('./m6');
 
 let regExpr = "a,bc";
 let o_M1 = M1(regExpr);
